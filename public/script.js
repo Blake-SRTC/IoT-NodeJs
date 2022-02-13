7c2789ddaee791bef75d6fa73f919fc42cdf60d8
Saul Tapia <rodrigo.1010stc@gmail.com>
2022-02-13 19:37:51 -0400

    Control de 3 Leds terminado

--- public/script.js
+++ public/script.js
@@ -8,7 +8,31 @@ document.getElementById('obj1').onclick = function() {
 
     } else {
 
-        socket.emit('lights', {status:"0"});
-        
+        socket.emit('lights', {status:"2"});
+
+    }
+}
+
+document.getElementById('obj2').onclick = function() {
+    if (document.getElementById('obj2').checked) {
+
+        socket.emit('lights', {status:"3"});
+
+    } else {
+
+        socket.emit('lights', {status:"4"});
+
+    }
+}
+
+document.getElementById('obj3').onclick = function() {
+    if (document.getElementById('obj3').checked) {
+
+        socket.emit('lights', {status:"5"});
+
+    } else {
+
+        socket.emit('lights', {status:"6"});
+
     }
 }
