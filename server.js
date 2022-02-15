721b8ae6f590e511dbc6c03543b0177af34f82c3
Saul Tapia <rodrigo.1010stc@gmail.com>
2022-02-15 18:48:42 -0400

    Probando Arduino + RBP3B+

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express(); // Start an express app
 const path = require('path') // Relative windows or Mac etc
 
+// RBP GPIO
+const Gpio = require('onoff').Gpio;
+
 // Variable server que contendra la app de express
 const server = http.createServer(app);
 
@@ -22,7 +25,7 @@ router.get('*', (req, res) => {
 // Definimos para poder usar router
 app.use("/", router);
 
-// Iniciamos el servidor
+// Iniciamos el servidor [este no usara sockets]
 // app.listen(5000, () => {
 //     console.log("App iniciada en puerto: ", 5000);
 // });
@@ -54,8 +57,48 @@ io.on('connection', function(socket) {
     socket.on('lights', function(data) {
         port.write(data.status);
         console.log(data);
+
+        // RBP GPIO
+        // console.log(typeof data.status);
     });
 });
 
 // Puerto de escucha Web
 server.listen(3000);
+
+
+
+// RBP GPIO control de los pines
+const LED1 = new Gpio(26, 'out');
+const LED2 = new Gpio(19, 'out');
+const LED3 = new Gpio(13, 'out');
+
+function rasp(status) {
+    if (status === '1') {
+        if (LED1.readSync() === 0) {
+            LED1.writeSync(1);
+        }
+    } else if (status === '2') {
+        if (LED1.readSync() === 1) {
+            LED1.writeSync(0);
+        }
+    }
+    if (status === '3') {
+        if (LED2.readSync() === 0) {
+            LED2.writeSync(1);
+        }
+    } else if (status === '4') {
+        if (LED2.readSync() === 1) {
+            LED2.writeSync(0);
+        }
+    }
+    if (status === '5') {
+        if (LED3.readSync() === 0) {
+            LED3.writeSync(1);
+        }
+    } else if (status === '6') {
+        if (LED3.readSync() === 1) {
+            LED3.writeSync(0);
+        }
+    }
+}
